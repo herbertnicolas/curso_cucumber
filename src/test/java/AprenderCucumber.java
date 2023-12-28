4f56da1ff9789d0f28396dcccc8a5e40e0f8f823
Herbert <hna@cin.ufpe.br>
2023-12-28 10:19:49 -0300

    adicionadas dependencias junit, criada classe runner com snippets e etc

---
+++ src/test/java/AprenderCucumber.java
@@ -0,0 +1,21 @@
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class AprenderCucumber {
+	@Dado("que criei o arquivo corretamente")
+	public void queCrieiOArquivoCorretamente() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+	@Quando("executá-lo")
+	public void executáLo() {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
+	@Então("a especificação deve finalizar com sucesso")
+	public void aEspecificaçãoDeveFinalizarComSucesso() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}	
+}
